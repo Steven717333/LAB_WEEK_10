323f450271a47eeb4277a7478f8737f4f1391013
Steven717333 <stevenlee313777@gmail.com>
2025-11-19 12:51:23 +0700

    Commit No. 1: Implement ViewModel to Handle Rotation

--- app/src/main/java/com/example/lab_week_10/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_10/MainActivity.kt
@@ -1,29 +1,39 @@
 package com.example.lab_week_10
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material3.Button
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.lab_week_10.viewmodels.TotalViewModel
 
 class MainActivity : AppCompatActivity() {
-    private var total: Int = 0
+
+    private val viewModel by lazy {
+        ViewModelProvider(this)[TotalViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        total = 0
-        updateText(total)
-        findViewById<Button>(R.id.button_increment).setOnClickListener {
-            incrementTotal()
-        }
+        prepareViewModel()
     }
-    private fun incrementTotal() {
-        total++
-        updateText(total)
-    }
+
     private fun updateText(total: Int) {
         findViewById<TextView>(R.id.text_total).text =
             getString(R.string.text_total, total)
+    }
+
+    private fun prepareViewModel() {
+        // FIXED
+//        viewModel.total.observe(this, Observer { total ->
+//            updateText(total)
+//        })
+        updateText(viewModel.total)
+
+        findViewById<Button>(R.id.button_increment).setOnClickListener {
+            viewModel.incrementTotal()
+            updateText(viewModel.total)
+        }
     }
 }
